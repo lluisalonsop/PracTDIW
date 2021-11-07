f1657383f0083b782408f5e8a8f7d02d950ff48c
SergiBons <57765195+SergiBons@users.noreply.github.com>
2021-11-07 16:53:36 +0100

    Add files via upload

--- public_html/index.php
+++ public_html/index.php
@@ -3,18 +3,19 @@
 
  switch ($accio) {
  case 'llistar-categories':
- include __DIR__.'/resource_llistar_categories.php';
+ include __DIR__.'/recursos/resource_llistar_categories.php';
  break;
 
- case 'llistar-productes'
- include __DIR__.'/resource_llistar_productes.php';
+ case 'llistar-productes':
+ include __DIR__.'/recursos/resource_llistar_productes.php';
  break;
 
- case 'mostrar-producte'
- include __DIR__.'/resource_mostrar_producte.php';
+ case 'mostrar-producte':
+ include __DIR__.'/recursos/resource_mostrar_producte.php';
  break;
 
  default:
- include __DIR__.'/resource_portada.php';
+ include __DIR__.'/recursos/resource_portada.php';
  break;
  }
+?>
---
+++ public_html/recursos/resource_portada.php
@@ -0,0 +1,11 @@
+<html lang="ca">
+<head>
+<title>Portada:</title>
+</head>
+<body>
+	<div class="portada">
+		<?php include __DIR__.'/controller/mostrar_portada.php'; 
+		?>
+	</div>
+</body>
+</html>
