37fd0b11af6a09072601113a946b34969ba89ca9
SergiBons <57765195+SergiBons@users.noreply.github.com>
2021-11-20 17:19:41 +0100

    Add files via upload

--- public_html/models/m_retorna_producte_detall.php
+++ public_html/models/m_retorna_producte_detall.php
@@ -1,8 +1,9 @@
 <?php
-function consultaResultats($connexio){
+function consultaResultats($connexio, $prod){
   try{
     $sql = "SELECT p.Nombre, p.Descripcion, p.Foto, p.Precio  
-      FROM productos p ";
+      FROM productos p 
+      WHERE p.ID = $prod";
     $consulta = $connexio->prepare($sql);
     $consulta->execute();
     $resultats_nom_categories = $consulta->fetchAll(PDO::FETCH_ASSOC);
