9064bb7b06fa37215530326745c3668b69f013d8
SergiBons <57765195+SergiBons@users.noreply.github.com>
2021-12-11 17:02:35 +0100

    Delete public_html directory

--- public_html/models/m_insertar_usuari.php
+++
@@ -1,7 +0,0 @@
-<?php 
-function insertaUsuari($conn, $user_id,$Nombre,$CPostal,$Adreca,$Correu,$Poblacio,$Clau){
-$sql = "INSERT INTO usuarios(ID, Nombre, Codigo_Postal, Direccion, Correo_Electronico,Poblacion,Password)
-VALUES ('$user_id','$Nombre','$CPostal','$Adreca','$Correu','$Poblacio','$Clau')";
-$conn->exec($sql);
-}
-?>
--- public_html/vistes/view_ajax_mostrar_producte.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	echo '<br/>';
-	echo '<div id ="replace">';
-	echo '<a> <img src="'.$res[0]['Foto'].'";/></a>';
-	echo '<p>'.$res[0]['Nombre'].' </p>';
-	echo '<p>'.$res[0]['Descripcion'].'</p>';
-	echo '<p>'.$res[0]['Precio'].'</p>';
-	echo '</div>';
-?>
