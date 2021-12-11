1a11bee3f5af1d0ed258a16be28b463e24108e8a
lluisalonsop <57227302+lluisalonsop@users.noreply.github.com>
2021-12-11 19:20:41 +0100

    Add files via upload

---
+++ public_html/resources/resource_portada.php
@@ -0,0 +1,11 @@
+<html lang="ca">
+<head>
+<title>Portada:</title>
+</head>
+<body>
+	<div class="portada">
+		<?php include __DIR__.'/../controladors/controller_portada.php'; 
+		?>
+	</div>
+</body>
+</html>
---
+++ public_html/resources/resource_set_login.php
@@ -0,0 +1,3 @@
+<?php 
+	include __DIR__.'/../controladors/controller_login.php';
+?>
