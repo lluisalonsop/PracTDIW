fbf620a3e722f224b5000c195bded2464f45d666
SergiBons <57765195+SergiBons@users.noreply.github.com>
2021-12-29 20:04:06 +0100

    Delete public_html directory

--- public_html/controladors/controller_afegeix_cabas.php
+++
@@ -1,8 +0,0 @@
-<?php
- include __DIR__.'/../models/m_conecta_BD.php';
- include __DIR__.'/../models/m_retorna_producte_detall.php';
-
- $ID = $_POST['prod'];
- $conn = conectaBD();
- $res = consultaResultats($conn, $ID);
-?>
