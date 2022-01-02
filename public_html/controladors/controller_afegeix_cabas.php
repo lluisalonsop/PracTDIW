f0a8c24eca431abff06e480ae6d4a2276f8ca4a0
lluisalonsop <57227302+lluisalonsop@users.noreply.github.com>
2022-01-02 17:42:51 +0100

    Delete public_html directory

--- public_html/controladors/controller_afegeix_cabas.php
+++
@@ -1,8 +0,0 @@
-<?php
- include __DIR__.'/../models/m_afegeix_cabas.php';
- $ID = $_GET['prod'];
- $PREU = $_GET['preu'];
- add_to_cart($ID, $PREU);
- include __DIR__.'/../vistes/view_ajax_mostrar_cistella.php';
-
-?>
