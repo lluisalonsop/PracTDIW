021a6465ce548acc19dae371350d81e46d4751ed
lluisalonsop <57227302+lluisalonsop@users.noreply.github.com>
2022-01-17 00:28:17 +0100

    Delete public_html directory

--- public_html/controladors/controller_buida_cistella.php
+++
@@ -1,6 +0,0 @@
-<?php
- @session_start();
- include __DIR__.'/../models/m_buida_cistella.php';
- include __DIR__.'/../vistes/view_mostrar_cabas.php'
-
-?>
--- public_html/recursosnew/js/cabas.js
+++
@@ -1,3 +0,0 @@
-function Buidaproduct(id,num,preu){
-	location.href = "/public_html/index.php?accio=Destruir&id="+id+"&preu="+preu+"&num="+num;
-}
