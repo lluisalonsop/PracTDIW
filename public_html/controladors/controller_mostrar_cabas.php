ff0a519b4763ab6aa04d350940bac7d1867777a7
lluisalonsop <57227302+lluisalonsop@users.noreply.github.com>
2022-01-16 20:00:24 +0100

    Delete public_html directory

--- public_html/controladors/controller_mostrar_cabas.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	include __DIR__.'/../models/m_conecta_BD.php';
-	$conn = conectaBD();
-	include __DIR__.'/../models/m_retorna_producte_detall.php';
-	include __DIR__.'/../vistes/view_mostrar_cabas.php';
-?>
